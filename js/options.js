23d9f6061a594151c87017526558a9adc187d389
jdonaghe <41173288+jdonaghe@users.noreply.github.com>
2018-07-13 13:46:04 +1000

    Update options.js

--- js/options.js
+++ js/options.js
@@ -26,7 +26,7 @@ $(document).ready(function () {
         if (
             $('#library').is(":checked")) {
             //change this url to match your library's! Here is Leiden's URL as an example for ExLibris Primo users - Old UI!
-            var newLibraryURL = 'http://catalogue.leidenuniv.nl/primo_library/libweb/action/search.do?fn=search&ct=search&initialSearch=true&mode=Basic&tab=all_content&indx=1&dum=true&srt=rank&vid=UBL_V1&frbg=&tb=t&scp.scps=scope%3A%28UBL_DSPACE%29%2Cscope%3A%28"UBL"%29%2Cscope%3A%28UBL_DTL%29%2Cscope%3A%28UBL_ALMA%29%2Cprimo_central_multiple_fe&vl%28freeText0%29=' + searchstring + '&source=LibrarySearchPluginPunblic'; //you can modofy &source to anything you like. This is useful to see how many users are coming to your catalogue via the plugin, in Google Analytics.
+            var newLibraryURL = 'https://apac-tc.hosted.exlibrisgroup.com/primo-explore/search?vid=BOND&lang=en_US' + searchstring + '&source=LibrarySearchPluginPunblic'; //you can modofy &source to anything you like. This is useful to see how many users are coming to your catalogue via the plugin, in Google Analytics.
             // Create the new tab
             chrome.tabs.create({
                 url: newLibraryURL
@@ -56,7 +56,7 @@ $(document).ready(function () {
         if (
             $('#pubmed').is(":checked")) {
             //alert("Google Scholar checked");
-            var newPubmedURL = 'https://ezproxy.someuniversity.com/login?url=https://www.ncbi.nlm.nih.gov/pubmed/?term=' + searchstring; // if you use EzProxy, modify the URL here. You may also remove the EzProxy prefix compeltely.
+            var newPubmedURL = 'https://www.ncbi.nlm.nih.gov/pubmed/?term=' + searchstring; // if you use EzProxy, modify the URL here. You may also remove the EzProxy prefix compeltely.
             // Create the new tab
             chrome.tabs.create({
                 url: newPubmedURL
@@ -74,7 +74,7 @@ $(document).ready(function () {
 var searches = [
     {
         title: "Search in the Library Catalogue", // Same as above: edit to match your library's url. Modify &soruce as well at the end. Remeber to keep '%s'
-        url: "http://catalogue.leidenuniv.nl/primo_library/libweb/action/search.do?fn=search&ct=search&initialSearch=true&mode=Basic&tab=all_content&indx=1&dum=true&srt=rank&vid=UBL_V1&frbg=&tb=t&scp.scps=scope%3A%28UBL_DSPACE%29%2Cscope%3A%28%22UBL%22%29%2Cscope%3A%28UBL_DTL%29%2Cscope%3A%28UBL_ALMA%29%2Cprimo_central_multiple_fe&vl%28freeText0%29=%s&source=LibrarySearchPluginPublic"
+        url: "https://apac-tc.hosted.exlibrisgroup.com/primo-explore/search?vid=BOND&lang=en_US&query=any,contains,life%s&source=LibrarySearchPluginPublic"
   },
     {
         title: "Search in WorldCat",
@@ -86,7 +86,7 @@ var searches = [
   },
     {
         title: "Search in PubMed", // Again: remove or adapt your ezproxy URL
-        url: "https://ezproxy.someuniversity.com/login?url=https://www.ncbi.nlm.nih.gov/pubmed/?term=%s"
+        url: "https://www.ncbi.nlm.nih.gov/pubmed/?term=%s"
   }
 ];
 // Create a parent item and two children.
